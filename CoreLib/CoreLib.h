a5f8da552dcc7d6a251378736184e58b25cc3411
Will Han <xingheng907@hotmail.com>
2014-12-03 18:57:43 +0800

    Added target CoreLib static library.

---
+++ CoreLib/CoreLib.h
@@ -0,0 +1,13 @@
+//
+//  CoreLib.h
+//  CoreLib
+//
+//  Created by WeiHan on 12/3/14.
+//  Copyright (c) 2014 Wei Han. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CoreLib : NSObject
+
+@end
---
+++ CoreLib/CoreLib.m
@@ -0,0 +1,13 @@
+//
+//  CoreLib.m
+//  CoreLib
+//
+//  Created by WeiHan on 12/3/14.
+//  Copyright (c) 2014 Wei Han. All rights reserved.
+//
+
+#import "CoreLib.h"
+
+@implementation CoreLib
+
+@end
